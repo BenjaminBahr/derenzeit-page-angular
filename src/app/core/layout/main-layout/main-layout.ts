e029b5fe628e61855539f429f17d1981b25f3edc
Benjamin Bahr <bbahr26@gmail.com>
2025-11-22 20:03:12 +0100

    Reduce warnings

--- src/app/core/layout/main-layout/main-layout.ts
+++ src/app/core/layout/main-layout/main-layout.ts
@@ -1,13 +1,11 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {Footer} from '../footer/footer';
 import {Header} from '../header/header';
 
 @Component({
   selector: 'app-main-layout',
   imports: [
     RouterOutlet,
-    Footer,
     Header
   ],
   templateUrl: './main-layout.html',
